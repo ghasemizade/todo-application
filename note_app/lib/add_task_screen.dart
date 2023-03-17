a253c416cefde77fcad4cf880d2489cae4785bba
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-03-17 17:52:16 +0330

    update code

--- note_app/lib/add_task_screen.dart
+++ note_app/lib/add_task_screen.dart
@@ -31,142 +31,145 @@ class _addTaskScreenState extends State<addTaskScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 15),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      _navigateToBackHome(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        backgroundColor: Color(0xff18DAA3),
-                        elevation: 5,
-                        shadowColor: Colors.black),
-                    child: Container(
-                      height: 60,
-                      width: 30,
-                      child: Icon(Icons.arrow_back_ios_rounded),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _navigateToBackHome(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          backgroundColor: Color(0xff18DAA3),
+                          elevation: 5,
+                          shadowColor: Colors.black),
+                      child: Container(
+                        height: 60,
+                        width: 30,
+                        child: Icon(Icons.arrow_back_ios_rounded),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: textFiledTaskTitle,
-                style: TextStyle(
-                    fontFamily: 'gm',
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600),
-                focusNode: wicher0,
-                decoration: InputDecoration(
-                  labelText: 'task',
-                  labelStyle: TextStyle(
-                    fontFamily: 'lato',
-                    fontSize: 28,
-                    color: wicher0.hasFocus
-                        ? Color(0xff18daa3)
-                        : Color(0xffc5c5c5),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+                ],
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  controller: textFiledTaskTitle,
+                  style: TextStyle(
+                      fontFamily: 'gm',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                  focusNode: wicher0,
+                  decoration: InputDecoration(
+                    labelText: 'task',
+                    labelStyle: TextStyle(
+                      fontFamily: 'lato',
+                      fontSize: 28,
+                      color: wicher0.hasFocus
+                          ? Color(0xff18daa3)
+                          : Color(0xffc5c5c5),
                     ),
-                    borderSide: BorderSide(
-                      color: Color(0xffc5c5c5),
-                      width: 3.0,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      borderSide: BorderSide(
+                        color: Color(0xffc5c5c5),
+                        width: 3.0,
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 3.0,
-                      color: Color(0xffc5c5c5),
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3.0,
+                        color: Color(0xffc5c5c5),
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: textFiledTaskSubTitle,
-                style: TextStyle(fontFamily: 'gm', fontSize: 18),
-                maxLines: 3,
-                focusNode: wicher1,
-                decoration: InputDecoration(
-                  labelText: 'description',
-                  labelStyle: TextStyle(
-                    fontFamily: 'lato',
-                    fontSize: 28,
-                    color: wicher1.hasFocus
-                        ? Color(0xff18daa3)
-                        : Color(0xffC5C5C5),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  controller: textFiledTaskSubTitle,
+                  style: TextStyle(fontFamily: 'gm', fontSize: 18),
+                  maxLines: 3,
+                  focusNode: wicher1,
+                  decoration: InputDecoration(
+                    labelText: 'description',
+                    labelStyle: TextStyle(
+                      fontFamily: 'lato',
+                      fontSize: 28,
+                      color: wicher1.hasFocus
+                          ? Color(0xff18daa3)
+                          : Color(0xffC5C5C5),
                     ),
-                    borderSide: BorderSide(
-                      color: Color(0xffc5c5c5),
-                      width: 3.0,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      borderSide: BorderSide(
+                        color: Color(0xffc5c5c5),
+                        width: 3.0,
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 3.0,
-                      color: Color(0xffc5c5c5),
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 3.0,
+                        color: Color(0xffc5c5c5),
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Spacer(),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: ElevatedButton(
-                onPressed: () {
-                  String taskTitle = textFiledTaskTitle.text;
-                  String taskSubTitle = textFiledTaskSubTitle.text;
-                  addTask(taskTitle, taskSubTitle);
-                },
-                child: Text(
-                  'Add Task',
-                  style: TextStyle(fontFamily: 'lato', fontSize: 20),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff18daa3),
-                  minimumSize: Size(200, 44),
+              Spacer(),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: ElevatedButton(
+                  onPressed: () {
+                    String taskTitle = textFiledTaskTitle.text;
+                    String taskSubTitle = textFiledTaskSubTitle.text;
+                    addTask(taskTitle, taskSubTitle);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Add Task',
+                    style: TextStyle(fontFamily: 'lato', fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff18daa3),
+                    minimumSize: Size(200, 44),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 38,
-            ),
-          ],
+              SizedBox(
+                height: 38,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- note_app/lib/home_screen.dart
+++ note_app/lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:note_app/add_task_screen.dart';
 import 'package:note_app/todo.dart';
@@ -20,29 +22,49 @@ class _homeScreenState extends State<homeScreen> {
   bool isChecked = false;
 
   var taskBox = Hive.box<Task>('taskBox');
+  bool isFabShow = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffE5E5E5),
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: ListView.builder(
-            itemCount: taskBox.values.length,
-            itemBuilder: ((context, index) {
-              var task = taskBox.values.toList()[index];
-              return taskWidget(task: task);
-            }),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _navigateToAddTask(context);
+          child: ValueListenableBuilder(
+        valueListenable: taskBox.listenable(),
+        builder: (context, value, child) {
+          return NotificationListener<UserScrollNotification>(
+            onNotification: (notification) {
+              setState(() {
+                if (notification.direction == ScrollDirection.forward) {
+                  isFabShow = true;
+                } else if (notification.direction == ScrollDirection.reverse) {
+                  isFabShow = false;
+                }
+              });
+              return true;
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: ListView.builder(
+                itemCount: taskBox.values.length,
+                itemBuilder: ((context, index) {
+                  var task = taskBox.values.toList()[index];
+                  return taskWidget(task: task);
+                }),
+              ),
+            ),
+          );
         },
-        backgroundColor: Color(0xff18DAA3),
-        child: Icon(Icons.add),
+      )),
+      floatingActionButton: Visibility(
+        visible: isFabShow,
+        child: FloatingActionButton(
+          onPressed: () {
+            _navigateToAddTask(context);
+          },
+          backgroundColor: Color(0xff18DAA3),
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
